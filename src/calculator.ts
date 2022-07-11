6831cb7aeb6052ed1814b29a4a2f9bf8c5822139
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2022-07-11 22:51:10 +0100

    chore: added TODOs for calculator

---
+++ src/calculator.ts
@@ -0,0 +1,7 @@
+// TODO: Implement these methods
+const calculator = {
+  // add(x: number, y: number): number {},
+  // subtract(x: number, y: number): number {},
+  // multiply(x: number, y: number): number {},
+  // divide(x: number, y: number): number {},
+};
---
+++ test/calculator.ts
@@ -0,0 +1,7 @@
+// TODO: Implement these these test first
+describe.skip('calculator', () => {
+  describe('add', () => {});
+  describe('subtract', () => {});
+  describe('multiply', () => {});
+  describe('divide', () => {});
+});
