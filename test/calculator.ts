4acb444445e9b1b20c6473ff1c5ee4278ae3098c
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2022-07-12 10:10:17 +0100

    test: add calculator.add tests

--- test/calculator.ts
+++ test/calculator.ts
@@ -1,7 +1,21 @@
+import { calculator } from '../src/calculator';
+
 // TODO: Implement these tests first
-describe.skip('calculator', () => {
-  describe('add', () => {});
-  describe('subtract', () => {});
-  describe('multiply', () => {});
-  describe('divide', () => {});
+describe('calculator', () => {
+  describe('add', () => {
+    it.each([
+      [0, 0, 0],
+      [1, 2, 3],
+      [9, 11, 20],
+      [0.5, 0.2, 0.7],
+    ])('should add %d and %d to get %d', (x, y, sum) => {
+      expect(calculator.add(x, y)).toEqual(sum);
+    });
+  });
+
+  describe.skip('subtract', () => {});
+
+  describe.skip('multiply', () => {});
+
+  describe.skip('divide', () => {});
 });
